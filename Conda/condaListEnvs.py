31f43fe4a531eaa636fb22a352d29de8f12dda2b
Blaine Mooers <bmooers1@gmail.com>
2021-01-28 20:21:51 -0600

    Updated format.

--- Conda/condaListEnvs.py
+++ Conda/condaListEnvs.py
@@ -1,6 +1,4 @@
-# Description:  List the currently available envs.
-The bang (!) enables running this command in a Juptyer Notebook.
-Delete the bang to run the command in the terminal.
+# Description:  List the currently available envs. The bang (!) enables running this command in a Juptyer Notebook. Delete the bang to run the command in the terminal.
 # Source:  NA
 
 """
--- Conda/condaRemoveEnv.py
+++ Conda/condaRemoveEnv.py
@@ -1,6 +1,4 @@
-# Description:  Remove a specified env.
-The bang (!) enables running this command in a Juptyer Notebook.
-Delete the bang to run the command in the terminal.
+# Description:  Remove a specified env. The bang (!) enables running this command in a Juptyer Notebook. Delete the bang to run the command in the terminal.
 # Source:  NA
 
 """
